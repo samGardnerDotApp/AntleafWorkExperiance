c0b473484569fa9544a1fd80dba8fa2adc973b47
Sam Gardner <gardner_s1@icloud.com>
2026-01-20 11:01:43 +0000

    Added scaleable table to template
    Added a table to the web app which changes length based on the amount of data using Jinja template statements. Added CSV interpretation to app.py and commented code in app.py.

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 import datetime
+import csv
 
+def csvToList(path): #Function which converts CSV into a list with each row stored as a list in the list
+    values = []
+    with open(path, mode ='r') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+                #print(lines)
+                values.append(lines)
+    return values
+
+data = csvToList('moistureData.csv') #Set data variable to contain the CSV data as lists
 
 app = Flask(__name__)
 
-date = datetime.datetime.now()
+date = datetime.datetime.now() #Sets date variable to current date and time
 
-@app.route("/")
+
+@app.route("/") #Sets path to this page. e.g. example.com/
 def hello_world():
-    return render_template("index.html", x=date)
+    return render_template("index.html", x=date, rows=int(len(data)), data=data) #Renders the Jinja template and passes data as data, the date as x and the number of rows in the table as rows
