a89897b6a12102845cfa2b3f243a96f6bba610ef
Sam Gardner <gardner_s1@icloud.com>
2026-01-20 11:28:33 +0000

    Added file downloading
    Removed unnecessary testing scripts, updated app.py to download the latest data from the google sheet. Updated variable names in Jinja template and gave web app title "Little Joe Dashboard".

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
 import datetime
 import csv
+from urllib.request import urlretrieve
+
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlrhEjK7N8t5rG0kyddsc40PY5xdliOVsMsoMq0Nd-CQ554JfyIcqpHbAHhOfnZvJuSiS3jD7agyEr/pub?gid=0&single=true&output=csv&urp=gmail_link" #URL of where the CSV is stored
+filename = "littleJoeSoilMoistureData.csv" #Name to save the retrieved CSV  under
+
+urlretrieve(url, filename) #Downloads the latest Little Joe soil moisture data to the working directory
 
 def csvToList(path): #Function which converts CSV into a list with each row stored as a list in the list
     values = []
@@ -11,7 +17,7 @@ def csvToList(path): #Function which converts CSV into a list with each row stor
                 values.append(lines)
     return values
 
-data = csvToList('moistureData.csv') #Set data variable to contain the CSV data as lists
+data = csvToList('littleJoeSoilMoistureData.csv') #Set data variable to contain the CSV data as lists
 
 app = Flask(__name__)
 
